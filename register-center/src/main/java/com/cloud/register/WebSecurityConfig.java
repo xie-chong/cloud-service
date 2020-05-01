05b141e6781bb80b835a87ea1fc2813735dd343d
xie-chong <xiechongyn@163.com>
2020-05-01 17:30:06 +0800

    eureka添加密码访问模式时才需要该配置

---
+++ register-center/src/main/java/com/cloud/register/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.cloud.register;
+
+/*
+// eureka添加密码访问模式时才需要该配置类
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+}
+
+*/
